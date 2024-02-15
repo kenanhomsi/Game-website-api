af703631b4698d15a0e51c76f393d87051a99eb4
kenanhomsi <kinanhomsi0959@gmail.com>
2024-02-15 18:59:40 +0300

    cors add3

--- app.js
+++ app.js
@@ -29,14 +29,17 @@ app.use('/api/v1/MainPage',MainPageRouter);
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const cors = require('cors');
+const helmet=require('helmet');
+const cros=require('cors');
+const xss=require('xss-clean');
 // const corsOptions ={
 //     origin:'http://localhost:5173', 
 //     credentials:true,            //access-control-allow-credentials:true
 //     optionSuccessStatus:200
 // }
-app.use(cors());
-
+app.use(helmet());
+app.use(cros());
+app.use(xss());
 
 const port = process.env.PORT || 10000;
 app.get('/', (req, res) => {
