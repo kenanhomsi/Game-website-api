7f22960119c6e21ed556bc957028e547481ff598
kenanhomsi <kinanhomsi0959@gmail.com>
2024-02-15 18:53:41 +0300

    cors add2

--- app.js
+++ app.js
@@ -30,12 +30,12 @@ app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
 const cors = require('cors');
-const corsOptions ={
-    origin:'http://localhost:5173', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+// const corsOptions ={
+//     origin:'http://localhost:5173', 
+//     credentials:true,            //access-control-allow-credentials:true
+//     optionSuccessStatus:200
+// }
+app.use(cors());
 
 
 const port = process.env.PORT || 10000;
